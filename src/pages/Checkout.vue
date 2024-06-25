8a739344179f811f5b773c561642e3f40bea7b24
fabioo.junioor <fj_011@hotmail.com>
2024-06-25 01:26:09 -0300

    feat: create pages 'home' and 'checkout'

---
+++ src/pages/Checkout.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div id="checkout">
+    <h2>checkout</h2>
+  </div>
+</template>
+
+<style scoped>
+#checkout{
+    background-color: grey;
+    height: 200px;
+    
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ProductCard from '../components/ProductCard.vue';
+
+</script>
+
+<template>
+  <div id="home" class="d-flex flew-wrap justify-center ga-3 pa-4">
+    <ProductCard
+      v-for="i in 3" :key="i" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
