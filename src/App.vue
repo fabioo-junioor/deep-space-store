2a17c18015a071adcf340f94c929f9d945ee153a
fabioo.junioor <fj_011@hotmail.com>
2024-06-25 01:27:55 -0300

    refator: settings

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
-
+  <div id="app">
+    <NavBar />
+    <router-view />
+  </div>
 </template>
 
 <style scoped>
-
+#app {
+  background-color: rgba(0, 0, 0, 0.1);
+  height: 100vh;
+  
+}
 </style>
