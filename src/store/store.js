5f3db18b1d3af4d807f99eaa11a87c9ba837595d
fabioo-junioor <fj_011@hotmail.com>
2024-06-26 09:24:53 -0300

    feat: create store with 'vuex

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,30 @@
 import { createStore } from 'vuex';
 
 const store = createStore({
-    state(){
-
+    state:{
+        dataFormPersonal: {name: '', email: '', fone:''},
+        dataFormDelivery : {zipCode: '', street: '', number: '', district: '', city: '', state: ''}
     },
     mutations: {
+        setDataFormPersonal(state, data){
+            state.dataFormPersonal = data;
 
+        },
+        setDataFormDelivery(state, data){
+            state.dataFormDelivery = data;
+
+        }
+    },
+    getters: {
+        getDataFormPersonal(state){
+            return state.dataFormPersonal;
+
+        },
+        getDataFormDelivery(state){
+            return state.dataFormDelivery;
+
+        }
     }
-});
+})
 
 export default store;
