ee61c66802ccfc694a65bd1267d15eec4456905c
fabioo.junioor <fj_011@hotmail.com>
2024-06-26 00:46:05 -0300

    feat: create form 'personal' and 'address' details

---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps(['rules', 'label', 'type', 'disabled']);
+const model = defineModel();
+
+</script>
+
+<template>
+  <div id="myInput">
+    <v-text-field
+        v-model="model"
+        :rules="rules"
+        :label="label"
+        :type="type"
+        :disabled="disabled">
+    </v-text-field>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
