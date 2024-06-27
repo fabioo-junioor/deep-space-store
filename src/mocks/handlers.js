60670da2f64c44c48dcd38bd86c6e2f8b6f473e1
fabioo.junioor <fj_011@hotmail.com>
2024-06-27 10:57:12 -0300

    feat: add handler product 'paymentMethods' and 'numberInstallments'

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -7,29 +7,38 @@ const handlers = [
             return HttpResponse.json({
                 offer_code: offer_code,
                 name: 'Tênis',
-                description: ' Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro.',
+                description: 'Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.',
                 price: 400,
-                img: 'https://images.vexels.com/content/262751/preview/sneakers-color-stroke-80s-482a17.png'
-            })
+                img: 'https://images.vexels.com/content/262751/preview/sneakers-color-stroke-80s-482a17.png',
+                paymentMethods: ['boleto', 'pix', 'cartao'],
+                numberInstallments: 2
+            }, {status: 200})
         }
         if(offer_code == '222'){
             return HttpResponse.json({
                 offer_code: offer_code,
                 name: 'Chinelo',
-                description: ' Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro.',
+                description: 'Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.',
                 price: 30,
-                img: 'https://cdn.b4c.inf.br/storage/fcwdistribuidora/1000/chinelo-havaianas-azul-naval-3940-1704828629.jpg'
-            })
+                img: 'https://cdn.b4c.inf.br/storage/fcwdistribuidora/1000/chinelo-havaianas-azul-naval-3940-1704828629.jpg',
+                paymentMethods: ['boleto', 'pix'],
+                numberInstallments: 4
+            }, {status: 200})
         }
         if(offer_code == '333'){
             return HttpResponse.json({
                 offer_code: offer_code,
                 name: 'Camiseta',
-                description: ' Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro.',
+                description: 'Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.',
                 price: 200,
-                img: 'https://nerdstore.com.br/wp-content/uploads/2022/10/b2b12f0f778cc0a0b1356a8170006c90.jpg'
-            })
+                img: 'https://nerdstore.com.br/wp-content/uploads/2022/10/b2b12f0f778cc0a0b1356a8170006c90.jpg',
+                paymentMethods: ['boleto'],
+                numberInstallments: 10
+            }, {status: 200})
         }
+        return HttpResponse.json({
+            error: 'Produto não encontrado'
+        })
 
     }),
     http.post('https://api.deepspacestore.com/offers/:offer_code/create_order', ({ request, params }) => {
