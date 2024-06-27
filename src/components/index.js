6f25f5dc311e82bab37d75a9d5ab6c24be14936c
fabioo-junioor <fj_011@hotmail.com>
2024-06-27 03:45:25 -0300

    refator: change to import components

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import FormPersonal from './forms/FormPersonal.vue';
+import FormDelivery from './forms/FormDelivery.vue';
+import FormPayment from './forms/FormPayment.vue';
+
+import CardCheckout from './cards/CardCheckout.vue';
+import ProductCardHome from './cards/ProductCardHome.vue';
+import ProductCardCheckout from './cards/ProductCardCheckout.vue'
+
+import NavBar from './NavBar.vue';
+
+export {
+    FormPersonal,
+    FormDelivery,
+    FormPayment,
+    CardCheckout,
+    ProductCardHome,
+    ProductCardCheckout,
+    NavBar
+}
